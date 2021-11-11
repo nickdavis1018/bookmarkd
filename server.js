724378ccc0b58d6fbd9d7c24bf214ebc64349938
Nicholas Davis <nickdavis1018@gmail.com>
2021-11-10 18:14:55 -0800

    setup backend

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const { PORT = 3000, MONGODB_URL } = process.env;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+
+
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  // Connection Events
+  mongoose.connection
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
+
+const BookmarkSchema = new mongoose.Schema({
+        website: String,
+        url: String,
+      });
+
+const Bookmark = mongoose.model("Bookmark", BookmarkSchema);
+
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+  });
